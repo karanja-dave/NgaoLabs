c748c2df27eb189f0c0921c1b06296eafea3207d
karanjadave <davekaranja6@gmail.com>
2026-02-21 00:54:24 +0300

    plotted hist and heatmap for numerical variables, defined preprocessing func (still not working)

--- Tanzania_tourism_prediction/code.py
+++ Tanzania_tourism_prediction/code.py
@@ -1,9 +1,11 @@
 ### import libraries for: 
 #EDA
 import pandas as pd 
-import pycountry 
-import pycountry_convert as pc
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+#preprocessing 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 ### Data preparation and cleaning 
 # wrangle function
@@ -18,6 +20,16 @@ def wrangle(path):
     #numerical variables 
     cols = ['total_female', 'total_male']
     df[cols] = df[cols].fillna(df[cols].median())
+    # drop high cardinality fetures 
+    df.drop(columns='country',inplace=True)
+
+    ## feature engineering 
+    #total people
+    # df["total_people"] = df["total_male"] + df["total_female"]
+    #total nights
+    # df["total_nights"] = df["night_mainland"] + df["night_zanzibar"]
+    #drop cols used in feat engineering to avid multicolllinearity
+    # df.drop(columns=['total_male','total_female','night_mainland','night_zanzibar'],inplace=True)
 
     return df 
 
@@ -25,9 +37,11 @@ def wrangle(path):
 
 # load data using wrangle func 
 train=wrangle('data/Train.csv')
+test=wrangle('data/Test.csv')
 
 
-
+train.select_dtypes(include='object').nunique()
+test.select_dtypes(include='object').nunique()
 
 ###EDA
 ##check data structure and format
@@ -38,6 +52,8 @@ train.info()
 train.isna().mean()>0.5 
 print('\n No variable has atleast 50% proportion of missing values.\n Imputation to be carried out on missing values\n')
 train.isna().sum()
+
+
 
 # examine cols with missing values individually 
 #categorical examintaion
@@ -54,13 +70,17 @@ train['total_female'].skew()
 train['total_male'].skew()
 print('\nBoth numerical columns are right skewed. Imputation should be done using median\n')
 
+
+# outliers 
+
+
 ## dealing with cardinality
 train.select_dtypes(include='object').nunique()
 train['country'].unique()
 print('Country has high cardinality and should be dropped. \n But the again, it seems to be an important feature in the model')
 
 # categorical analysis based on cardinality 
-cate_var = train.select_dtypes(include="object").drop(columns=['ID','country'])
+cate_var = train.select_dtypes(include="object")
 for col in cate_var:
     print(f"{col}: {train[col].nunique()} unique values")
     print(train[col].value_counts(dropna=False))
@@ -68,4 +88,59 @@ for col in cate_var:
 
 print('\nfrom the analysis, I can observe that some categorical cols are ordered while other are not.\nThis implies that both label and one hot encoding will be applied to repsctive categorical variables\n')
 train.info()
- 
+
+##Visualization
+#dealing with multicollinearity
+corr= train.select_dtypes("float64").corr()
+sns.heatmap(corr)
+plt.show()
+
+print("No multicollinearity is observed betweeen any pair of feature variables")
+# histogram : Distribution of numerical cols 
+num_cols = train.select_dtypes(include="float64")
+train[num_cols.columns].hist(bins=30, figsize=(14,10))
+plt.tight_layout()
+plt.show()
+
+num_cols.describe()
+
+pd.read_csv('data/Train.csv')['total_cost'].head()
+
+# data preprocessing 
+# define categorical and label cols
+categ_cols = train.select_dtypes(include='string').columns.drop('age_group')
+label_cols = ["age_group"]
+
+#init transformers
+ohe = OneHotEncoder(drop='first', sparse_output=False)
+le_feature = {col: LabelEncoder() for col in label_cols}
+
+def feature_preprocessing(data, fit=True):
+    data = data.drop(columns=['ID'], errors='ignore')
+    
+    #label encoding
+    for col in label_cols:
+        if fit:
+            le_feature[col].fit(data[col].astype(str))
+        data[col] = le_feature[col].transform(data[col].astype(str))
+    
+    # OHE
+    if fit:
+        ohe_array = ohe.fit_transform(data[categ_cols])
+    else:
+        ohe_array = ohe.transform(data[categ_cols])
+    
+    ohe_df = pd.DataFrame(
+        ohe_array,
+        columns=ohe.get_feature_names_out(categ_cols),
+        index=data.index
+    )
+    
+    # Drop original categorical columns and concatenate OHE
+    data = data.drop(columns=categ_cols)
+    data = pd.concat([data, ohe_df], axis=1)
+    
+    return data
+
+feature_preprocessing(train, fit=True)
+feature_preprocessing(test, fit=False)
