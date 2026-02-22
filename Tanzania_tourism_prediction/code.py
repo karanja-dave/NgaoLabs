fe0c44efd47fd419f480fc7c836cd810854213a8
karanjadave <davekaranja6@gmail.com>
2026-02-22 21:15:42 +0300

    ensured preprocessing func is working, encoded country, split target&feature, applied preprocessing func and split train and validation sets

--- Tanzania_tourism_prediction/code.py
+++ Tanzania_tourism_prediction/code.py
@@ -3,9 +3,14 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 #preprocessing 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+
+
 
 ### Data preparation and cleaning 
 # wrangle function
@@ -21,15 +26,26 @@ def wrangle(path):
     cols = ['total_female', 'total_male']
     df[cols] = df[cols].fillna(df[cols].median())
     # drop high cardinality fetures 
-    df.drop(columns='country',inplace=True)
+    # df.drop(columns='country',inplace=True)
 
     ## feature engineering 
     #total people
-    # df["total_people"] = df["total_male"] + df["total_female"]
+    df["total_people"] = df["total_male"] + df["total_female"]
     #total nights
-    # df["total_nights"] = df["night_mainland"] + df["night_zanzibar"]
+    df["total_nights"] = df["night_mainland"] + df["night_zanzibar"]
+
+    # count total packages 
+    pkg_cols = df.filter(like='package_').columns #select cols with package name 
+    df['package_count'] = (df[pkg_cols] == 'Yes').sum(axis=1)
+
+    # ratios
+    df['mainland_ratio'] = df['night_mainland'] / (df['total_nights'] + 1e-6)
+    df['zanzibar_ratio'] = df['night_zanzibar'] / (df['total_nights'] + 1e-6)
+    
     #drop cols used in feat engineering to avid multicolllinearity
-    # df.drop(columns=['total_male','total_female','night_mainland','night_zanzibar'],inplace=True)
+    df.drop(columns=list(pkg_cols)+['total_male', 'total_female', 'night_mainland', 'night_zanzibar'],inplace=True)
+    #drop one ratio to avoid multicollinearity as they have very high correlation
+    df.drop(columns='zanzibar_ratio',inplace=True)
 
     return df 
 
@@ -77,7 +93,8 @@ print('\nBoth numerical columns are right skewed. Imputation should be done usin
 ## dealing with cardinality
 train.select_dtypes(include='object').nunique()
 train['country'].unique()
-print('Country has high cardinality and should be dropped. \n But the again, it seems to be an important feature in the model')
+print('\nCountry has high cardinality and should be dropped. \n But the again, it seems to be an important feature in the model')
+print('\nWe will not be droppping country but we will be transforming it into numerical values using `Cross-Validated Target Encoding` explained well in the documentation')
 
 # categorical analysis based on cardinality 
 cate_var = train.select_dtypes(include="object")
@@ -89,13 +106,16 @@ for col in cate_var:
 print('\nfrom the analysis, I can observe that some categorical cols are ordered while other are not.\nThis implies that both label and one hot encoding will be applied to repsctive categorical variables\n')
 train.info()
 
+# dealing with incosistencies in the `age_group` col 
+test['age_group'] = test['age_group'].replace({'24-Jan': '1-24'})
+
 ##Visualization
 #dealing with multicollinearity
 corr= train.select_dtypes("float64").corr()
 sns.heatmap(corr)
 plt.show()
 
-print("No multicollinearity is observed betweeen any pair of feature variables")
+print("\n No multicollinearity is observed betweeen any pair of feature variables\n However theres a high correlation between the engineered ratios, so dropped one of them\n")
 # histogram : Distribution of numerical cols 
 num_cols = train.select_dtypes(include="float64")
 train[num_cols.columns].hist(bins=30, figsize=(14,10))
@@ -106,9 +126,36 @@ num_cols.describe()
 
 pd.read_csv('data/Train.csv')['total_cost'].head()
 
-# data preprocessing 
+### data preprocessing 
+##encoding the country column
+#train
+train['country_encoded'] = np.nan
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+for train_idx, val_idx in kf.split(train):
+    train_fold = train.iloc[train_idx]
+    val_fold = train.iloc[val_idx]
+    
+    country_mean = train_fold.groupby('country')['total_cost'].mean()
+    
+    train.loc[val_idx, 'country_encoded'] = val_fold['country'].map(country_mean)
+
+# fill missing/unseen categories with global mean
+train['country_encoded']=train['country_encoded'].fillna(train['total_cost'].mean())
+# drop `country` col
+train.drop(columns='country',inplace=True)
+
+#test encoding of country column
+test['country_encoded'] = test['country'].map(country_mean)
+#fill missing values 
+test['country_encoded']=test['country_encoded'].fillna(train['total_cost'].mean())
+# drop `country`col 
+test.drop(columns='country',inplace=True)
+
+
+
 # define categorical and label cols
-categ_cols = train.select_dtypes(include='string').columns.drop('age_group')
+categ_cols = train.select_dtypes(include='string').columns.drop(['age_group', 'ID'])
 label_cols = ["age_group"]
 
 #init transformers
@@ -142,5 +189,16 @@ def feature_preprocessing(data, fit=True):
     
     return data
 
-feature_preprocessing(train, fit=True)
-feature_preprocessing(test, fit=False)
+#split target
+X_train=train.drop(['total_cost'],axis=1)
+y_train=train['total_cost']
+
+#encoding trnasformation
+encoded_train=feature_preprocessing(X_train, fit=True)
+encoded_test=feature_preprocessing(test, fit=False)
+
+
+# split train and validation sets 
+X_Train1, X_val, y_Train, y_val = train_test_split(
+    encoded_train, y_train, test_size=0.1, random_state=42
+)
